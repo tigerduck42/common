c129a203299aeb5c38d400c69088c435e9735767
Gregor <gregor-j@users.noreply.github.com>
2018-12-28 13:15:00 +0100

    add tests for abstract remote function call

---
+++ tests/AbstractRemoteFunctionCallTest.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * File src/AbstractRemoteFunctionCallTest.php
+ *
+ * Test the abstract remote function call.
+ *
+ * @package common
+ * @author  Gregor J.
+ * @license MIT
+ */
+
+namespace tests\phpsap\classes;
+
+use kbATeam\TypeCast\TypeCastArray;
+use kbATeam\TypeCast\TypeCastValue;
+use phpsap\classes\AbstractFunction;
+use phpsap\classes\AbstractRemoteFunctionCall;
+use phpsap\classes\ConnectionPool;
+use phpsap\exceptions\ConnectionNotFoundException;
+use phpsap\interfaces\IFunction;
+use tests\phpsap\classes\helper\ConfigA;
+use tests\phpsap\classes\helper\Connection;
+use tests\phpsap\classes\helper\RemoteFunction;
+use tests\phpsap\classes\helper\RemoteFunctionCall;
+
+/**
+ * Class tests\phpsap\classes\AbstractRemoteFunctionCallTest
+ *
+ * Test the abstract remote function call.
+ *
+ * @package tests\phpsap\classes
+ * @author  Gregor J.
+ * @license MIT
+ */
+class AbstractRemoteFunctionCallTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test class inheritance.
+     */
+    public function testInheritance()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        static::assertInstanceOf(IFunction::class, $rfc);
+        static::assertInstanceOf(AbstractRemoteFunctionCall::class, $rfc);
+        static::assertInstanceOf(RemoteFunctionCall::class, $rfc);
+    }
+
+    /**
+     * Test getting a function instance.
+     */
+    public function testGetFunction()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        $rfc->returnName = 'awvovkms';
+        $function = &$rfc->getFunction();
+        static::assertInstanceOf(IFunction::class, $function);
+        static::assertInstanceOf(AbstractFunction::class, $function);
+        static::assertInstanceOf(RemoteFunction::class, $function);
+        static::assertSame('awvovkms', $function->getName());
+    }
+
+    /**
+     * Test using the same connection config twice which should create only one
+     * connection.
+     */
+    public function testDuplicateConnectionInPool()
+    {
+        /**
+         * Create a connection from a config and add it to the connection pool.
+         */
+        $config = new ConfigA();
+        $connection = new Connection($config);
+        ConnectionPool::add($connection->getId(), $connection);
+        /**
+         * Create a new RFC from the same config, which should add the same
+         * connection ID to the pool.
+         */
+        $rfc = new RemoteFunctionCall($config);
+        /**
+         * Now remove the connection ID we created and see if the rfc runs into an
+         * exception.
+         */
+        ConnectionPool::remove($connection->getId());
+        $this->setExpectedException(
+            ConnectionNotFoundException::class,
+            sprintf(
+                'Connection ID \'%s\' does not exist.',
+                $connection->getId()
+            )
+        );
+        $rfc->getFunction();
+    }
+
+    /**
+     * Test setting a parameter.
+     */
+    public function testSetParam()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        $rfc->setParam('mddaudvn', 613);
+        $function = $rfc->getFunction();
+        static::assertSame(['mddaudvn' => 613], $function->debugGet('params'));
+    }
+
+    /**
+     * Test resetting a function call.
+     */
+    public function testReset()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        $rfc->setParam('yovgwyfi', 51.3);
+        $rfc->reset();
+        $function = $rfc->getFunction();
+        static::assertSame([], $function->debugGet('params'));
+    }
+
+    /**
+     * Test invoking a remote function call without parameters.
+     */
+    public function testInvokeWithoutTypecast()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        $rfc->getFunction()->results = ['gpgtowzq' => 'C5AWVD1h'];
+        $results = $rfc->invoke();
+        static::assertSame(['gpgtowzq' => 'C5AWVD1h'], $results);
+    }
+
+    public function testInvokeWithTypecast()
+    {
+        $rfc = new RemoteFunctionCall(new ConfigA());
+        $rfc->getFunction()->results = ['rjgcdeyl' => "6.19"];
+        $rfc->returnTypecast = new TypeCastArray();
+        $rfc->returnTypecast['rjgcdeyl'] = new TypeCastValue('float');
+        $results = $rfc->invoke();
+        static::assertArrayHasKey('rjgcdeyl', $results);
+        static::assertSame(6.19, $results['rjgcdeyl']);
+    }
+}
---
+++ tests/helper/RemoteFunctionCall.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * File tests/helper/RemoteFunctionCall.php
+ *
+ * Helper class extending the abstract remote function class for testing.
+ *
+ * @package common
+ * @author  Gregor J.
+ * @license MIT
+ */
+
+namespace tests\phpsap\classes\helper;
+
+use phpsap\classes\AbstractRemoteFunctionCall;
+use phpsap\interfaces\IConfig;
+
+/**
+ * Class tests\phpsap\classes\helper\RemoteFunctionCall
+ *
+ * Helper class extending the abstract remote function class for testing.
+ *
+ * @package tests\phpsap\classes\helper
+ * @author  Gregor J.
+ * @license MIT
+ */
+class RemoteFunctionCall extends AbstractRemoteFunctionCall
+{
+    /**
+     * @var string function name
+     */
+    public $returnName = 'cketfemo';
+
+    /**
+     * @var null|\kbATeam\TypeCast\ITypeCast
+     */
+    public $returnTypecast;
+
+    /**
+     * The SAP remote function name.
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->returnName;
+    }
+
+    /**
+     * Get the typecast of the expected return values.
+     * @return \kbATeam\TypeCast\ITypeCast|null
+     */
+    protected function getReturnTypecast()
+    {
+        return $this->returnTypecast;
+    }
+
+    /**
+     * Create a connection instance using the given config.
+     * @param \phpsap\interfaces\IConfig $config
+     * @return \phpsap\interfaces\IConnection
+     */
+    protected function createConnectionInstance(IConfig $config)
+    {
+        return new Connection($config);
+    }
+
+    /**
+     * Make protected function public for testing.
+     * Get the function instance.
+     * @return \tests\phpsap\classes\helper\RemoteFunction
+     * @throws \phpsap\interfaces\IConnectionFailedException
+     * @throws \phpsap\interfaces\IUnknownFunctionException
+     */
+    public function &getFunction()
+    {
+        return parent::getFunction();
+    }
+}
